aa9840a46c05a12e6a7479666048e27e7bd81573
Bruno Alves <bruno6821@gmail.com>
2022-11-11 16:04:49 -0300

    Alterando Context Complexo

--- context_api/src/pages/Home.js
+++ context_api/src/pages/Home.js
@@ -9,13 +9,23 @@ const Home = () => {
   // const { counter } = useContext(CounterContext);
   const { counter } = useCounterContext();
 
-  const { color } = useTitleColorCOntext();
+  const { color, dispatch } = useTitleColorCOntext();
+
+  const setTitleColor = (color) => {
+    dispatch({ type: color });
+  };
 
   return (
     <div>
       <h1 style={{ color: color }}>Home</h1>
       <p>Valor do Contador: {counter}</p>
       <ChangeCounter />
+
+      <div>
+        <button onClick={() => setTitleColor("RED")}>Vermelho</button>
+        <button onClick={() => setTitleColor("BLUE")}>Azul</button>
+      </div>
+
     </div>
   );
 };
